35ed477f788cebb00a4a02e7d735060537848eaf
Maksym Lupekha <2627number2@gmail,com>
2024-09-05 13:13:52 +0300

    solution

--- app/create_file.py
+++ app/create_file.py
@@ -1,10 +1,11 @@
-import sys
 import os
+import sys
 from datetime import datetime
 
 
 def create_file() -> None:
     args = sys.argv[1:]
+
     if not args:
         raise ValueError("No arguments provided")
 
@@ -13,15 +14,14 @@ def create_file() -> None:
 
     if "-d" in args:
         d_flag = args.index("-d")
-        if "-f" in args:
-            f_flag = args.index("-f")
-            dir_path = args[d_flag + 1:f_flag]
-            file_name = args[f_flag + 1]
-        else:
-            dir_path = args[d_flag + 1:]
-    elif "-f" in args:
-        f_index = args.index("-f")
-        file_name = args[f_index + 1]
+        dir_path = args[d_flag + 1:]
+        if "-f" in dir_path:
+            f_flag = dir_path.index("-f")
+            dir_path = dir_path[:f_flag]
+
+    if "-f" in args:
+        f_flag = args.index("-f")
+        file_name = args[f_flag + 1]
 
     if dir_path:
         os.makedirs(os.path.join(*dir_path), exist_ok=True)
@@ -35,6 +35,7 @@ def create_file() -> None:
         with open(file_path, "a") as file:
             timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
             file.write(f"\n{timestamp}\n")
+
             line_number = 1
             while True:
                 content = input(f"Enter content line {line_number}: ")
